664eee2b79cd18af30fc98ce40599a52938c8bbd
tenie <tenie@tenie.net>
2023-04-27 18:33:35 +0800

    up

--- app/src/test/java/net/tenie/fx/test/window/Main.java
+++ app/src/test/java/net/tenie/fx/test/window/Main.java
@@ -1,6 +1,7 @@
 package net.tenie.fx.test.window;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -14,6 +15,7 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
+import net.tenie.Sqlucky.sdk.utility.CommonUtility;
 
 public class Main extends Application {
 	 
@@ -22,12 +24,14 @@ public class Main extends Application {
     private static boolean isBottomRight;// 是否处于右下角调整窗口状态
     private static boolean isBottom;// 是否处于下边界调整窗口状态
     private static boolean isTop;
+    private static boolean isLeft;
+    private static boolean isTopLeft;
+    
     private final static int RESIZE_WIDTH = 5;// 判定是否为调整窗口状态的范围与边界距离
     private final static double MIN_WIDTH = 300;// 窗口最小宽度
     private final static double MIN_HEIGHT = 250;// 窗口最小高度
     private double xOffset = 0;
     private double yOffset = 0;
-    static boolean topLock = false;
     @Override
     public void start(Stage stage) throws Exception{
 //        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -77,7 +81,7 @@ public class Main extends Application {
             Cursor cursorType = Cursor.DEFAULT;// 鼠标光标初始为默认类型，若未进入调整窗口状态，保持默认类型
  
             // 先将所有调整窗口状态重置
-            isTop = isRight = isBottomRight = isBottom = false;
+            isLeft =  isTop = isRight = isBottomRight = isBottom = false;
             if (y >= height - RESIZE_WIDTH) {
                 if (x <= RESIZE_WIDTH) {// 左下角调整窗口状态
  
@@ -88,15 +92,21 @@ public class Main extends Application {
                     isBottom = true;
                     cursorType = Cursor.S_RESIZE;
                 }
-            } else if (x >= width - RESIZE_WIDTH) {// 右边界调整窗口状态
+            }
+            else if (x >= width - RESIZE_WIDTH) {// 右边界调整窗口状态
                 isRight = true;
                 cursorType = Cursor.E_RESIZE;
-            }else if( RESIZE_WIDTH >= y ) {
-            	 System.out.println(y );
+            }
+            else if( RESIZE_WIDTH >= y ) {
+//            	 System.out.println(y );
             	 cursorType = Cursor.S_RESIZE;
             	 isTop = true;
             }
-            
+			else if (RESIZE_WIDTH >= x) {
+				System.out.println(x);
+				cursorType = Cursor.E_RESIZE;
+				isLeft = true;
+			}
             
             
             // 最后改变鼠标光标
@@ -108,21 +118,27 @@ public class Main extends Application {
       
         
         root.setOnMouseDragged(event -> {
+        	// 鼠标x y 坐标
             double x = event.getSceneX();
             double y = event.getSceneY();
-            // 保存窗口改变后的x、y坐标和宽度、高度，用于预判是否会小于最小宽度、最小高度
+            
+            
+            // 窗口的x, y ;  保存窗口改变后的x、y坐标和宽度、高度，用于预判是否会小于最小宽度、最小高度
             double nextX = stage.getX();
             double nextY = stage.getY();
             double stageY = stage.getY();
+            double stageX = stage.getX();
             
             double nextWidth = stage.getWidth();
             double nextHeight = stage.getHeight();
             
             double stageHeight = stage.getHeight();
+            double stageWidth = stage.getWidth();
             
             double topNextHeight = 0.0;
+            double leftNextWidth = 0.0;
  
-            if( isRight || isBottomRight || isBottomRight || isBottom ) {
+            if( isRight || isBottomRight || isBottom ) {
             	 if (isRight || isBottomRight) {// 所有右边调整窗口状态
                      nextWidth = x;
                  }
@@ -148,36 +164,70 @@ public class Main extends Application {
             
            
             
-            if( isTop && topLock == false) { 
-            	topLock = true;
+            if( isTop || isLeft ) {
             	topNextHeight = stageHeight - y;
+            	leftNextWidth = stageWidth - x;
            
                 if (topNextHeight >= viewHeight) {// 如果窗口改变后的高度小于最小高度，则高度调整到最小高度
-                	topNextHeight = viewHeight;
+                	return;
                 }
                 if( topNextHeight < 0 ) {
                 	return;
                 }
-                stage.setX(nextX);
+                
+                if (leftNextWidth >= viewWidth) {// 如果窗口改变后的高度小于最小高度，则高度调整到最小高度
+                	return;
+                }
+                if( leftNextWidth < 0 ) {
+                	return;
+                }
+                 
                 
                 double tmpY  = topNextHeight - nextHeight;
-	           	 tmpY = stageY - tmpY;
-	           	 if(tmpY <0 ) {
+                double tmpX = leftNextWidth - nextWidth;
+ 	           	 tmpY = stageY - tmpY;
+ 	           	 tmpX = stageX - tmpX;
+ 	           	 if(tmpY < 0 || tmpY > viewHeight) {
+ 	           		 return;
+ 	           	 } 
+ 	             if(tmpX < 0 || tmpX > viewWidth) {
 	           		 return;
-	           	 }else if(tmpY > viewHeight) {
-	           		 return;
+	           	 } 
+ 	           	 
+ 	           	 if( topNextHeight <  MIN_HEIGHT) {
+ 	           		 return ;
+ 	           	 }
+ 	             if( leftNextWidth <  MIN_WIDTH) {
+	           		 return ;
 	           	 }
-	           	 
-	           	 stage.setY(tmpY);
-	           	 
-                stage.setWidth(nextWidth);
-                stage.setHeight(topNextHeight);
-//                System.out.println("isRight = " + isRight + " | isBottom = " + isBottom + " | isBottomRight = " + isBottomRight);
+ 	           	 
+                final double Ytmp  = tmpY;
+//                final double Widthnext = nextWidth;
+            	 
+                final double Xtmp  = tmpX;
+//                final double Widthnext = nextWidth;
+                
+                
+                final double NextHeighttop = topNextHeight;
+
+                final double NextWidthLeft = leftNextWidth;
                 
                 System.out.println("y = " +y+ " | tmpY = "+tmpY+" | topNextHeight = " + topNextHeight);
-                topLock = false;
+                CommonUtility.delayRunThread(v->{
+                	 Platform.runLater(()->{
+                		  stage.setX(Xtmp); 
+         	           	  stage.setY(Ytmp);
+         	           	  
+                         stage.setWidth(NextWidthLeft);
+                         stage.setHeight(NextHeighttop);
+                         
+                        
+                   });
+                }, 100);
+               
+               
             }
-
+            
 
             
             
