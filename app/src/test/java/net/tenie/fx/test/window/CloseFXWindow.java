2b56d76b438b323821fee8d02cd772b41e1c680b
tenie <tenie@tenie.net>
2023-05-26 18:26:58 +0800

    up

--- app/src/test/java/net/tenie/fx/test/window/CloseFXWindow.java
+++ app/src/test/java/net/tenie/fx/test/window/CloseFXWindow.java
@@ -1,4 +1,12 @@
 package net.tenie.fx.test.window;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.jfoenix.svg.SVGGlyph;
+import com.jfoenix.svg.SVGGlyphLoader;
+
 import javafx.application.Application;
 import javafx.stage.*;
 import javafx.scene.*;
@@ -11,14 +19,21 @@ public class CloseFXWindow extends Application {
     Application.launch(CloseFXWindow.class, args);
   }
  
-  @Override public void start(final Stage stage) {
+  @Override public void start(final Stage stage) throws IOException {
+	  
+	  
+		SVGGlyphLoader sl = new SVGGlyphLoader();
+		File svgFile = new File("D:\\myGit\\Learning_Notes\\图标\\s.svg");
+		URL url=svgFile.toURL();
+		
+		SVGGlyph svg = sl.loadGlyph(url);
     stage.setTitle("FX Close");
     
     Button close = new Button("Close");
     close.setOnAction(e->{
     	Event.fireEvent(stage, new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST ));
     });
-    
+    close.setGraphic(svg);
     
     Button hidden = new Button("hidden");
     hidden.setOnAction(e->{
